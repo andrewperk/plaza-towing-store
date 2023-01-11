91c7945b7eb2cb0ed98dfa9fff5b5937095d559f
Andrew Perkins <andrewperk@gmail.com>
2023-01-10 20:50:00 -0800

    First commit

---
+++ PlazaTowingStore/PlazaTowingStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlazaTowingStoreApp.swift
+//  PlazaTowingStore
+//
+//  Created by Andrew L Perkins on 1/10/23.
+//
+
+import SwiftUI
+
+@main
+struct PlazaTowingStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
